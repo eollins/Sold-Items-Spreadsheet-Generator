6f8ae08bff18e99ae47120ef9c95467ca9c19f8b
Ethan Ollins <ethanollins6@gmail.com>
2018-03-03 15:02:43 -0800

    First attempt at revised method

--- Sold Items Spreadsheet Generator/Form1.cs
+++ Sold Items Spreadsheet Generator/Form1.cs
@@ -18,6 +18,7 @@ namespace Sold_Items_Spreadsheet_Generator
         Dictionary<string, List<SpreadsheetItem>> itemsByYear = new Dictionary<string, List<SpreadsheetItem>>();
         Dictionary<string, List<SpreadsheetItem>> itemsByQuality = new Dictionary<string, List<SpreadsheetItem>>();
         Dictionary<string, List<SpreadsheetItem>> itemsByBoth = new Dictionary<string, List<SpreadsheetItem>>();
+        List<string> names = new List<string>();
 
         List<SpreadsheetItem> items = new List<SpreadsheetItem>();
         public Form1()
@@ -91,24 +92,13 @@ namespace Sold_Items_Spreadsheet_Generator
 
                     SpreadsheetItem item = new SpreadsheetItem(components[0], components[1], components[2], components[3], components[4], int.Parse(components[5]), components[6], components[7], components[8], components[9], components[10]);
 
-                    if (!itemsByYear.ContainsKey(item.Year))
-                    {
-                        itemsByYear.Add(item.Year, new List<SpreadsheetItem>());
-                    }
-
-                    itemsByYear[item.Year].Add(item);
-
-                    if (!itemsByQuality.ContainsKey(item.Quality))
-                    {
-                        itemsByQuality.Add(item.Quality, new List<SpreadsheetItem>());
-                    }
-
                     if (!itemsByBoth.ContainsKey(item.Year + " " + item.Quality))
                     {
                         itemsByBoth.Add(item.Year + " " + item.Quality, new List<SpreadsheetItem>());
                     }
 
                     itemsByBoth[item.Year + " " + item.Quality].Add(item);
+                    names.Add(item.Player);
 
                     progressBar1.Value++;
                     label1.Text = progressBar1.Value + " items grouped";
@@ -118,21 +108,62 @@ namespace Sold_Items_Spreadsheet_Generator
 
                 textBox1.Text = "Searching eBay Databases";
 
+                List<SearchResult> itemsFound = new List<SearchResult>();
                 foreach (string category in itemsByBoth.Keys)
                 {
-                    //switch (category)
-                    //{
-                    //    case ""
-                    //}
+                    progressBar2.Maximum = names.Count;
 
-                    WebRequest request = WebRequest.Create("http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findCompletedItems&SERVICE-VERSION=1.7.0&SECURITY-APPNAME=GregoryM-mailer-PRD-a45ed6035-97c14545&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&keywords=" + "" + "&categoryId=213&categoryId=214&categoryId=215&categoryId=216&categoryId=37795&listingType=Auction&paginationInput.entriesPerPage=200");
-                    WebResponse response = await request.GetResponseAsync();
+                    //Tomorrow:
+                    //Find ways to determine quality from listing
+                    //Organize accordingly
 
-                    string xml = await new StreamReader(response.GetResponseStream()).ReadToEndAsync();
-                    XmlDocument doc = new XmlDocument();
-                    doc.LoadXml(xml);
+                    int numberOfPages = 2;
+                    for (int i = 1; i < numberOfPages; i++)
+                    {
+                        WebRequest request = WebRequest.Create("http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findCompletedItems&SERVICE-VERSION=1.7.0&SECURITY-APPNAME=GregoryM-mailer-PRD-a45ed6035-97c14545&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&keywords=" + category.Split(' ')[0] + "&categoryId=213&categoryId=214&categoryId=215&categoryId=216&categoryId=37795&itemFilter(0).name=Professional%20Grader&itemFilter(0).value=Not%20Professionally%20Graded&listingType=Auction&paginationInput.entriesPerPage=200&paginationInput.pageNumber=" + i);
+                        WebResponse response = await request.GetResponseAsync();
 
-                    XmlNodeList nodes = ((XmlElement)((XmlElement)doc.GetElementsByTagName("findCompletedItemsResponse")[0]).GetElementsByTagName("searchResult")[0]).GetElementsByTagName("item");
+                        string xml = await new StreamReader(response.GetResponseStream()).ReadToEndAsync();
+                        XmlDocument doc = new XmlDocument();
+                        doc.LoadXml(xml);
+
+                        numberOfPages = int.Parse(((XmlElement)((XmlElement)doc.GetElementsByTagName("findCompletedItemsResponse")[0]).GetElementsByTagName("paginationOutput")[0]).GetElementsByTagName("totalPages")[0].InnerText);
+
+                        XmlNodeList responseItems = ((XmlElement)((XmlElement)doc.GetElementsByTagName("findCompletedItemsResponse")[0]).GetElementsByTagName("searchResult")[0]).GetElementsByTagName("item");
+                        foreach (XmlElement el in responseItems)
+                        {
+                            string title = el.GetElementsByTagName("title")[0].InnerText.ToLower();
+                            
+                            SearchResult result = new SearchResult();
+                            if (title.Contains(category.Split(' ')[1]))
+                                result.Year = int.Parse(category.Split(' ')[1]);
+                            result.SoldPrice = decimal.Parse(((XmlElement)el.GetElementsByTagName("sellingStatus")[0]).GetElementsByTagName("convertedCurrentPrice")[0].InnerText);
+
+                            if (title.Contains("goudey sport kings"))
+                            {
+                                title.Replace("goudey sport kings", " ");
+                            }
+
+                            foreach (string player in names)
+                            {
+                                if (title.Contains(player))
+                                { 
+                                    result.Player = player;
+                                    break;
+                                }
+                            }
+
+                            progressBar2.Value++;
+
+                            if (result.Player != null)
+                            {
+                                itemsFound.Add(result);
+                                MessageBox.Show("item found");
+                            }
+                        }
+                    }
+
+                    
                         
                         
 
@@ -147,208 +178,3 @@ namespace Sold_Items_Spreadsheet_Generator
         }
     }
 }
-
-/*int count = 0;
-                foreach (string item in lines)
-                {
-                    if (count == 0)
-                    {
-                        count++;
-                        continue;
-                    }
-                    
-                    string[] components = item.Split(',');
-
-                    SpreadsheetItem spreadsheetItem = new SpreadsheetItem("", "", "", "", "", 0, "", "", "", "", "");
-                    try
-                    {
-                        string four = "";
-                        string twelve = "";
-                        if (components.Length == 13)
-                        {
-                            spreadsheetItem = new SpreadsheetItem(components[0], components[1], components[2], components[3], components[4] + " " + components[5], int.Parse(components[6]), components[7], components[8], components[9], components[10], components[11] + " " + components[12]);
-                        }
-                        else
-                        {
-                            spreadsheetItem = new SpreadsheetItem(components[0], components[1], components[2], components[3], components[4], int.Parse(components[5]), components[6], components[7], components[8], components[9], components[10]);
-                        }
-                    }
-                    catch (Exception ex)
-                    {
-
-                    }
-
-                    int category = 0;
-                    switch (spreadsheetItem.Sport)
-                    {
-                        case "Baseball":
-                            category = 213;
-                            break;
-                        case "Basketball":
-                            category = 214;
-                            break;
-                        case "Football":
-                            category = 215;
-                            break;
-                        case "Hockey":
-                            category = 216;
-                            break;
-                        case "Boxing":
-                            category = 37795;
-                            break;
-                        default:
-                            category = -1;
-                            break;
-                    }
-
-                    string keywords = spreadsheetItem.Player + " " + spreadsheetItem.Year + " " + spreadsheetItem.Set;
-                    string link = "http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findCompletedItems&SERVICE-VERSION=1.7.0&SECURITY-APPNAME=GregoryM-mailer-PRD-a45ed6035-97c14545&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&keywords=" + keywords + "&categoryId=" + category + "&listingType=Auction&paginationInput.entriesPerPage=200";
-                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(link);
-                    Log.Items.Add("Created web request for " + keywords);
-
-                    WebResponse response = null;
-                    if (category != -1)
-                    {
-                        try
-                        {
-                            response = await request.GetResponseAsync();
-                        }
-                        catch (Exception ex)
-                        {
-                            if (ex.Message.Contains("Internal Server Error"))
-                            {
-                                try
-                                {
-                                    Log.Items.Add("Error. Retrying in 5 seconds");
-                                    Thread.Sleep(5000);
-                                    response = await request.GetResponseAsync();
-                                }
-                                catch
-                                {
-                                    Log.Items.Add("Error. Retrying in 10 seconds");
-                                    bool finished = false;
-                                    DateTime start = DateTime.Now;
-                                    while (!finished)
-                                    {
-                                        Thread.Sleep(10000);
-                                        try
-                                        {
-                                            response = await request.GetResponseAsync();
-                                            finished = true;
-                                        }
-                                        catch
-                                        {
-                                            if ((DateTime.Now - start).TotalMinutes >= 5)
-                                            {
-                                                MessageBox.Show("Calls could not be made.");
-                                                Application.Exit();
-                                            }
-                                        }
-                                    }
-                                }
-
-                                Log.Items.Add("Web request for " + keywords + " failed");
-                                continue;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        continue;
-                    }
-
-                    bool completed = true;
-
-                    Log.Items.Add("Requested items for " + keywords);
-
-                    if (completed)
-                    {
-                        XmlDocument doc = new XmlDocument();
-                        string xml = await new StreamReader(response.GetResponseStream()).ReadToEndAsync();
-                        doc.LoadXml(xml);
-
-                        XmlNodeList nodes = ((XmlElement)((XmlElement)doc.GetElementsByTagName("findCompletedItemsResponse")[0]).GetElementsByTagName("searchResult")[0]).GetElementsByTagName("item");
-                        int amount = int.Parse(((XmlElement)doc.GetElementsByTagName("findCompletedItemsResponse")[0]).GetElementsByTagName("searchResult")[0].Attributes[0].Value);
-
-                        Log.Items.Add("Found " + amount + " results for " + keywords);
-
-                        if (amount == 0)
-                            continue;
-
-                        List<XmlElement> itemsToCheck = new List<XmlElement>();
-                        if (graded)
-                        {
-                            foreach (XmlElement el in nodes)
-                            {
-                                try
-                                {
-                                    WebRequest getSingleItems = WebRequest.Create("http://open.api.ebay.com/shopping?callname=GetSingleItem&responseencoding=XML&appid=GregoryM-mailer-PRD-a45ed6035-97c14545&siteid=0&version=967&ItemID=" + el.GetElementsByTagName("itemId")[0].InnerText + "&IncludeSelector=ItemSpecifics");
-                                    WebResponse gsiResp = await getSingleItems.GetResponseAsync();
-                                    string responseXml = await new StreamReader(gsiResp.GetResponseStream()).ReadToEndAsync();
-                                    XmlDocument r = new XmlDocument();
-                                    r.LoadXml(responseXml);
-                                    XmlNodeList nodes2 = ((XmlElement)((XmlElement)((XmlElement)r.GetElementsByTagName("GetSingleItemResponse")[0]).GetElementsByTagName("Item")[0]).GetElementsByTagName("ItemSpecifics")[0]).GetElementsByTagName("NameValueList");
-                                    foreach (XmlElement ele in nodes2)
-                                    {
-                                        if (ele.GetElementsByTagName("Name")[0].InnerText == "Grade")
-                                        {
-                                            itemsToCheck.Add(el);
-                                        }
-                                    }
-                                }
-                                catch (Exception ex)
-                                {
-                                    MessageBox.Show(ex.Message);
-                                }
-                            }
-                        }
-                        else
-                        {
-                            foreach (XmlElement el in nodes)
-                            {
-                                itemsToCheck.Add(el);
-                            }
-                        }
-
-                        List<decimal> prices = new List<decimal>();
-                        foreach (XmlElement el in itemsToCheck)
-                        {
-                            decimal price = decimal.Parse(((XmlElement)doc.GetElementsByTagName("sellingStatus")[0]).GetElementsByTagName("currentPrice")[0].InnerText);
-                            prices.Add(price);
-                        }
-
-                        decimal high = 0;
-                        foreach (decimal price in prices)
-                        {
-                            if (price > high)
-                            {
-                                high = price;
-                            }
-                        }
-
-                        decimal low = 99999999;
-                        foreach (decimal price in prices)
-                        {
-                            if (price < low)
-                            {
-                                low = price;
-                            }
-                        }
-
-                        decimal avg = 0;
-                        foreach (decimal price in prices)
-                        {
-                            avg += price;
-                        }
-                        avg = avg / prices.Count;
-
-                        spreadsheetItem.HighPrice = high.ToString("C0");
-                        spreadsheetItem.LowPrice = low.ToString("C0");
-                        spreadsheetItem.AvgPrice = avg.ToString("C0");
-
-                        items.Add(spreadsheetItem);
-                        progressBar1.Value++;
-                        label1.Text = progressBar1.Value + " items added";
-                        Log.SelectedIndex = Log.Items.Count - 1;
-*/
-  
