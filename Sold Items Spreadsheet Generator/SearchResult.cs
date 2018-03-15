2dce891bfe0cf822cfcfdd61ee05d6164f55a707
Ethan Ollins <ethanollins6@gmail.com>
2018-03-14 18:01:18 -0700

    Begin revision

---
+++ Sold Items Spreadsheet Generator/SearchResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sold_Items_Spreadsheet_Generator
+{
+    class SearchResult
+    {
+        public int Year { get; set; }
+        public string Player { get; set; }
+        public string Quality { get; set; }
+        public decimal SoldPrice { get; set; }
+    }
+}
